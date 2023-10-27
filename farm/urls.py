075ecc21eff932a6b2f98e1b663c962df4466366
NjeruKariuki <freakoutbond2@gmail.com>
2023-10-27 15:40:09 +0300

    created endpoints for farm

--- farm/urls.py
+++ farm/urls.py
@@ -1,5 +1,36 @@
 from django.urls import path
+from .views import (
+    ServiceCreateView,
+    QuestionCreateView,
+    LivestockProduceView,
+    CropProduceView,
+    FarmActivityCreateView,
+    AnswerView,
+    ServiceListView,
+    QuestionListView,
+    FarmActivityListView
+)
 
 urlpatterns = [
+    # URL for booking a service
+    path('services/book/', ServiceCreateView.as_view(), name='book-service'),
+    path('services/', ServiceListView.as_view(), name='services'),
 
+    # URL for asking a question
+    path('questions/ask/', QuestionCreateView.as_view(), name='ask-question'),
+    path('questions/', QuestionListView.as_view(), name='questions'),
+
+  # URL for planning a farm activity
+    path('farmactivity/plan/', FarmActivityCreateView.as_view(), name='create-farm-activity'),
+    path('farmactivity/', FarmActivityListView.as_view(), name='farmactivity'),
+
+
+    # URL for viewing and adding produce from livestock
+    path('livestock-produce/', LivestockProduceView.as_view(), name='livestock-produce'),
+
+    # URL for viewing and adding produce from crop
+    path('crop-produce/', CropProduceView.as_view(), name='crop-produce'),
+
+    # URL for viewing and adding answers
+    path('questions/answers/', AnswerView.as_view(), name='answers'),
 ]
--- farm/views.py
+++ farm/views.py
@@ -27,44 +27,59 @@ from .serializers import (
 # View for booking a service
 class ServiceCreateView(generics.CreateAPIView):
     serializer_class = ServiceSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         # Set the farmer booking the service to the current user
         serializer.save(farmer=self.request.user)
 
+#all services available
+class ServiceListView(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
 # View for asking a question
 class QuestionCreateView(generics.CreateAPIView):
     serializer_class = QuestionSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         # Set the farmer asking the question to the current user
         serializer.save(farmer=self.request.user)
 
+class QuestionListView(generics.ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
 # View for viewing and adding produce from livestock
 class LivestockProduceView(generics.ListCreateAPIView):
     queryset = Livestock.objects.all()
     serializer_class = LivestockSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
 # View for viewing and adding produce from crop
 class CropProduceView(generics.ListCreateAPIView):
     queryset = Crop.objects.all()
     serializer_class = CropSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
 # View for planning a farm activity
 class FarmActivityCreateView(generics.CreateAPIView):
     serializer_class = FarmActivitySerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         # Set the farmer planning the activity to the current user
         serializer.save(farmer=self.request.user)
 
+#all services available
+class FarmActivityListView(generics.ListAPIView):
+    queryset = FarmActivity.objects.all()
+    serializer_class = FarmActivitySerializer
+
 # View for viewing and adding answers
 class AnswerView(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
