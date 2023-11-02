5d4dcaf21f3fb323d3529cabe82bce1eba5c2427
NjeruKariuki <freakoutbond2@gmail.com>
2023-11-02 12:55:26 +0300

    fetched data for cards and chart

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,94 +1,103 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import ActivityTable from "@/components/ActivityTable";
 import Card from "@/components/Card";
-import LineChart from "@/components/MyChart";
 import Navbar from "@/components/Navbar";
 import NumberedListTable from "@/components/ServiceTable";
-import BarChart from "@/components/MyChart";
+import MyChart from "@/components/MyChart";
 import api from '../../utils/api';
 
-import MyChart from "@/components/MyChart";
-
-
-
 export default function Dashboard() {
-  //get current user details
-  const email = localStorage.getItem("email");
-  const username = localStorage.getItem("username");
-  const userDetails = { "email": email, "username": username }
-  // console.log(username);
+    const [livestockData, setLivestockData] = useState([]);
+    const [cropData, setCropData] = useState([]);
+    const [storageData, setStorageData] = useState([]);
+    const [storeData, setStoreData] = useState([]);
 
-  //get cards details
-  //number of livestock
-  const fetchData = async (endpoint) => {
-    try {
-      const res = await api.get(endpoint);
-      //access data from response
-      const data = res.data;
-      console.log('data: ', data);
-      return data;
-    } catch (err) {
-      console.log('Error fetching data:', err)
-    }
-  };
+    // Get current user details
+    const email = localStorage.getItem("email");
+    const username = localStorage.getItem("username");
 
-  //fetching livestock data
-  const livestockData = fetchData('farm/livestock/');
-  // total = livestockData.reduce((accumulator, currentValue) => accumulator + parseInt(currentValue.number, 10), 0);
-  //fetching crop data
-  const cropData = fetchData('farm/crop/');
-  const storeData = fetchData('farm/stores/');
-  const storageData = fetchData('farm/storages/');
+    // Fetch data when the component mounts
+    useEffect(() => {
+        const fetchData = async (endpoint, stateSetter) => {
+            try {
+                const res = await api.get(endpoint);
+                // Access data from response
+                const data = res.data;
+                stateSetter(data);
+            } catch (err) {
+                console.log('Error fetching data:', err);
+            }
+        };
+
+        // Fetch livestock data
+        fetchData('farm/livestock/', setLivestockData);
+        // Fetch crop data
+        fetchData('farm/crop/', setCropData);
+        fetchData('farm/stores/', setStoreData);
+        fetchData('farm/storages/', setStorageData);
+    }, []);
+
+    //set store and storage data
+    const store = storeData[0] ? storeData[0] : null;
+    const storage = storeData[0] ? storeData[0] : null;
+
+    //get produce of crops and livestock
+    
+
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100">
+            <Navbar />
+            <div className="w-full px-4 py-12 lg:pl-80">
+                <div className="h-full">
+                    <div className="flex items-center w-full h-16 topBar">
+                        <h1 className="font-bold uppercase">Welcome Back, {username}</h1>
+                    </div>
+                    <section className="grid grid-cols-1 gap-4 mainSection">
+                        <section className="topSection">
+                            <div className="flex flex-col">
+                                <h3 className="font-medium uppercase">Overview</h3>
+                                <div className="grid gap-4 md:grid-cols-4 sm:grid-cols-2 cardsContainer">
+                                    <Card label="Number of Livestock" details={livestockData.length} />
+                                    <Card label="Number of crops" details={cropData.length} />
+                                    {store && (
+                                        <Card label="Store Capacity" details={`${store.used_capacity}/${store.capacity}`} />
+                                    )}
+
+                                    {storage && (
+                                        <Card label="Storage Capacity" details={`${storage.used_capacity}/${storage.capacity}`} />
+
+                                    )}
 
 
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      <Navbar />
-      <div className="w-full px-4 py-12 lg:pl-80">
-        <div className="h-full">
-          <div className="flex items-center w-full h-16 topBar">
-            <h1 className="font-bold uppercase">Welcome Back, {username}</h1>
-          </div>
-          <section className="grid grid-cols-1 gap-4 mainSection">
-            <section className="topSection">
-              <div className="flex flex-col">
-                <h3 className="font-medium uppercase">Overview</h3>
-                <div className="grid gap-4 md:grid-cols-4 sm:grid-cols-2 cardsContainer">
-                  <Card label="Number of Livestock" details='34' />
-                  <Card label="Number of crops" details="2,334" />
-                  <Card label="Store Capacity" details="10/100" />
-                  <Card label="Storage Capacity" details="12/50" />
+                                </div>
+                            </div>
+                        </section>
+                        <section className="grid grid-cols-1 md:grid-cols-2 bottomSection">
+                            <div className="w-full p-4 chartDiv">
+                                <h1 className="font-medium uppercase">Produce over time</h1>
+                                {/* Replace with your chart component */}
+                                <MyChart />
+                            </div>
+                            <div className="w-full p-4 detailsContainer">
+                                <div className="services">
+                                    <h1 className="font-medium uppercase">Services Requested</h1>
+                                    <div className="mt-4">
+                                        <NumberedListTable service="Ploughing" provider="Makiss Ltd" date="18-12-2023" />
+                                    </div>
+                                </div>
+                                <div className="w-full p-4 farmActivities">
+                                    <h1 className="font-medium uppercase">Upcoming Farm Activities</h1>
+                                    <div className="mt-4">
+                                        <ActivityTable activity="Weeding" date="12-05-2023" />
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </section>
                 </div>
-              </div>
-            </section>
-            <section className="grid grid-cols-1 md:grid-cols-2 bottomSection">
-              <div className="w-full p-4 chartDiv">
-                <h1 className="font-medium uppercase">Produce over time</h1>
-
-                {/* Replace with your chart component */}
-                <MyChart />
-
-              </div>
-              <div className="w-full p-4 detailsContainer">
-                <div className="services">
-                  <h1 className="font-medium uppercase">Services Requested</h1>
-                  <div className="mt-4">
-                    <NumberedListTable service="Ploughing" provider="Makiss Ltd" date="18-12-2023" />
-                  </div>
-                </div>
-                <div className="w-full p-4 farmActivities">
-                  <h1 className="font-medium uppercase">Upcoming Farm Activities</h1>
-                  <div className="mt-4">
-                    <ActivityTable activity="Weeding" date="12-05-2023" />
-                  </div>
-                </div>
-              </div>
-            </section>
-          </section>
+            </div>
         </div>
-      </div>
-    </div>);
+    );
 }
-
--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -3,26 +3,64 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { useState, useEffect } from 'react';
+import api from '../utils/api';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const MyChart = () => {
-    const [selectedTimePeriod, setSelectedTimePeriod] = useState('Today');
+    const [selectedTimePeriod, setSelectedTimePeriod] = useState(1);
+    const [livestockProduceData, setLivestockProduceData] = useState([]);
+    const [cropProduceData, setCropProduceData] = useState([]);
 
     const timePeriods = {
-        'Today': [12, 19],
-        'Last 7 Days': [50, 60],
-        'Last 1 Month': [10, 180],
-        'Last 3 Months': [600, 540],
-        'Last 1 Year': [180, 2160],
+        1: [12, 19],
+        7: [50, 60],
+        30: [10, 180],
+        90: [600, 540],
+        365: [180, 2160],
     };
+
+
+
+
+    useEffect(() => {
+        const updateChartData = (period) => {
+            chartData.datasets[0].data = timePeriods[period];
+        };
+
+        const fetchData = async (endpoint, stateSetter) => {
+            try {
+                const res = await api.get(endpoint);
+                // Access data from response
+                const data = res.data;
+                stateSetter(data);
+            } catch (err) {
+                console.log('Error fetching data:', err);
+            };
+        };
+
+        updateChartData(selectedTimePeriod);
+        fetchData(`farm/produce/?last_days=${selectedTimePeriod}&content_type=17`, setLivestockProduceData);
+        fetchData(`farm/produce/?last_days=${selectedTimePeriod}&content_type=19`, setCropProduceData);
+    }, [selectedTimePeriod]);
+
+    console.log(cropProduceData);
+    console.log(livestockProduceData);
+
+    const totalOfLivestockProduce = livestockProduceData.reduce((total, item) => total + item.quantity, 0);
+
+    console.log('livestock', totalOfLivestockProduce);
+
+    const totalOfCropProduce = cropProduceData.reduce((total, item) => total + item.quantity, 0);
+
+    console.log('crop', totalOfCropProduce);
 
     const chartData = {
         labels: ['Produce from Crops', 'Produce from Livestock'],
         datasets: [
             {
                 label: 'Quantity Produced',
-                data: timePeriods[selectedTimePeriod],
+                data: [totalOfCropProduce, totalOfLivestockProduce],
                 backgroundColor: ['rgba(64, 192, 87, 0.5)', 'rgba(165, 42, 42, 0.6)'],
                 borderColor: ['rgba(75, 192, 192, 1)', 'rgba(165, 42, 42, 1)'],
                 borderWidth: 1,
@@ -30,54 +68,43 @@ const MyChart = () => {
         ],
     };
 
-    useEffect(() => {
-        const updateChartData = (period) => {
-            chartData.datasets[0].data = timePeriods[period];
-        };
-
-        updateChartData(selectedTimePeriod);
-    }, [selectedTimePeriod]);
+    //get produce crop
 
     return (
         <div className="w-full">
             <div className="grid justify-center grid-cols-2 gap-2 py-2 mb-4 lg:grid-cols-5 md:grid-cols-3">
                 <button
-                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${
-                        selectedTimePeriod === 'Today' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
-                    }`}
-                    onClick={() => setSelectedTimePeriod('Today')}
+                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${selectedTimePeriod === 'Today' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
+                        }`}
+                    onClick={() => setSelectedTimePeriod(1)}
                 >
                     Today
                 </button>
                 <button
-                    className={`px-4 py-2 md:px-3 sm:px-2 mr-2 rounded-lg focus:outline-none ${
-                        selectedTimePeriod === 'Last 7 Days' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
-                    }`}
-                    onClick={() => setSelectedTimePeriod('Last 7 Days')}
+                    className={`px-4 py-2 md:px-3 sm:px-2 mr-2 rounded-lg focus:outline-none ${selectedTimePeriod === 'Last 7 Days' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
+                        }`}
+                    onClick={() => setSelectedTimePeriod(7)}
                 >
                     Last 7 Days
                 </button>
                 <button
-                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${
-                        selectedTimePeriod === 'Last 1 Month' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
-                    }`}
-                    onClick={() => setSelectedTimePeriod('Last 1 Month')}
+                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${selectedTimePeriod === 'Last 1 Month' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
+                        }`}
+                    onClick={() => setSelectedTimePeriod(30)}
                 >
                     Last 1 Month
                 </button>
                 <button
-                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${
-                        selectedTimePeriod === 'Last 3 Months' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
-                    }`}
-                    onClick={() => setSelectedTimePeriod('Last 3 Months')}
+                    className={`px-4 py-2 mr-2 rounded-lg focus:outline-none ${selectedTimePeriod === 'Last 3 Months' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
+                        }`}
+                    onClick={() => setSelectedTimePeriod(90)}
                 >
                     Last 3 Months
                 </button>
                 <button
-                    className={`px-4 py-2 rounded-lg focus:outline-none ${
-                        selectedTimePeriod === 'Last 1 Year' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
-                    }`}
-                    onClick={() => setSelectedTimePeriod('Last 1 Year')}
+                    className={`px-4 py-2 rounded-lg focus:outline-none ${selectedTimePeriod === 'Last 1 Year' ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-600'
+                        }`}
+                    onClick={() => setSelectedTimePeriod(365)}
                 >
                     Last 1 Year
                 </button>
